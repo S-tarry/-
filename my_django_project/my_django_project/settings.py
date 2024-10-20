48cf7b521a0428374dc36cb766f43e75eb48e152
Starry <jonb62031@gmail.com>
2024-10-20 12:05:18 +0300

    add new app

--- my_django_project/my_django_project/settings.py
+++ my_django_project/my_django_project/settings.py
@@ -47,6 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
+    'games',
+    'posts',
+    'orders',
+    'payments',
 ]
 
 MIDDLEWARE = [
